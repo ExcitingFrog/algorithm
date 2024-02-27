2cd703262a8c03c04c46d02fc0d8928fba2468b8
xingo <398303159@qq.com>
2024-02-27 10:10:28 +0800

    feat: add dp

---
+++ leetcode/1143-dp(LCS).go
@@ -0,0 +1,19 @@
+package leetcode
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range m + 1 {
+		dp[i] = make([]int, n+1)
+	}
+	for i, c1 := range text1 {
+		for j, c2 := range text2 {
+			if c1 == c2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
